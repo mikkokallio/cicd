638be78d2b1b76f14b33b04348e952cc76207f88
Mikko Kallio <mikkokallio@microsoft.com>
2021-09-03 23:51:31 +0300

    New repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# backend
+
+View the UI at: https://shielded-citadel-76663.herokuapp.com/
---
+++ build/precache-manifest.e4f896e1b699312bda6e578638ecb0cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a5050c14154802d2a9c31f35382d968",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc97e4db7c9e1d3d4b38",
+    "url": "/static/css/main.efc48fce.chunk.css"
+  },
+  {
+    "revision": "d72cc7c3f59543947486",
+    "url": "/static/js/2.029bf66b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.029bf66b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc97e4db7c9e1d3d4b38",
+    "url": "/static/js/main.4f064e6b.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);

30e280e4b500b16cc9482b991a2a8386fe5e151a
Mikko Kallio <mikkokallio@microsoft.com>
2021-09-04 01:04:19 +0300

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
   'env': {
     'node': true,
-    "browser": true,
+    'browser': true,
     'commonjs': true,
     'es2020': true
   },
   'extends': 'eslint:recommended',
   'parserOptions': {
     'ecmaVersion': 11,
-    "sourceType": "module"
+    'sourceType': 'module'
   },
   'rules': {
     'eqeqeq': 'error',

ac4f574398e4ae80fcb7dba6b812ed137798202c
Mikko Kallio <mikkokallio@microsoft.com>
2021-09-04 00:54:10 +0300

    a

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,8 @@ module.exports = {
   },
   'extends': 'eslint:recommended',
   'parserOptions': {
-    'ecmaVersion': 11
+    'ecmaVersion': 11,
+    "sourceType": "module"
   },
   'rules': {
     'eqeqeq': 'error',

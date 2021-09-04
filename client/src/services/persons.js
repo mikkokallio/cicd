d51de93def3c8aa3a1e262770be89561d0b73aa5
Mikko Kallio <mikkokallio@microsoft.com>
2021-09-04 12:29:53 +0300

    prod env option

--- client/src/services/persons.js
+++ client/src/services/persons.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
-//const baseUrl = 'https://shielded-citadel-76663.herokuapp.com/api/persons'
-const baseUrl = 'http://localhost:3001/api/persons'
+
+let baseUrl
+process.env.NODE_ENV === 'prod' ? baseUrl = `https://reallyboringapp.herokuapp.com/api/persons` : baseUrl = 'http://localhost:3001/api/persons'
+//const baseUrl = 'http://localhost:3001/api/persons'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
